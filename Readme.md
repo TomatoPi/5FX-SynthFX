27c4813625110bbd61aea4370f48b3cda923a809
DAGO Kokri Esaïe <dago.esaie@protonmail.com>
2021-02-10 13:58:38 +0100

    :tada: First Commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# 5FX-SynthFX
+Synthesiser written in C++ using JACK and NSM
+
+## Dependencies
+
+- jack <https://jackaudio.org/>
+- liblo <http://liblo.sourceforge.net/>
---
+++ src/SynthFX.cpp
@@ -0,0 +1,12 @@
+#include <jack/jack.h>
+#include <jack/midiport.h>
+
+static int jack_callback(jack_nframes_t nframes, void* args) {
+
+  return 0;
+}
+
+int main(int argc, char const* argv[]) {
+  /* code */
+  return 0;
+}
